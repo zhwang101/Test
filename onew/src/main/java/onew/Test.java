79affe298f7a7c00e3161c45b4f4dcb8ae8be47f
wang <836242565@qq.com>
2018-02-03 17:29:38 +0800

    seven

--- onew/src/main/java/onew/Test.java
+++ onew/src/main/java/onew/Test.java
@@ -8,11 +8,9 @@ public class Test {
      System.out.println("two");
      System.out.println("three");
 
-System.out.println("chon1");
-
-
-
+     System.out.println("chon1");
      System.out.println("four");
+     System.out.println("seven");
 
 	}
 
