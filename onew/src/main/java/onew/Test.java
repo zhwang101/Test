97bb08234f4df89a88f93b7a3b1cf357aa1cb77a
wang <836242565@qq.com>
2018-02-03 17:02:49 +0800

    ggg

--- onew/src/main/java/onew/Test.java
+++ onew/src/main/java/onew/Test.java
@@ -8,11 +8,8 @@ public class Test {
      System.out.println("two");
      System.out.println("three");
 
-
      System.out.println("four");
      System.out.println("five");
-     System.out.println("ss");
-     
 
 	}
 
