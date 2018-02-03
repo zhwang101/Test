c6e18748ca970b7146387e3c073bbb620711ef9b
wang <836242565@qq.com>
2018-02-03 17:45:20 +0800

    yy2

--- onew/src/main/java/onew/Test.java
+++ onew/src/main/java/onew/Test.java
@@ -7,12 +7,11 @@ public class Test {
      System.out.println("one");
      System.out.println("two");
      System.out.println("three");
-
-     System.out.println("chon1");
+    
      System.out.println("four");
      System.out.println("seven");
-     System.out.println("douyu");
-
+     
+     System.out.println("yy");
 	}
 
 }
